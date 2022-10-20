92fd581bc961d503270de833f9b8f86073ea5d48
Andrew Huynh <tanphuqn@gmail.com>
2022-10-20 06:16:11 -0400

    Init

---
+++ http-clients.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func CallNbconvert() {
+	resp, err := http.Get("http://localhost:8080/nbconvert")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
+
+func TestCallNbconvert(t *testing.T) {
+	for i := 0; i < 2; i++ {
+		CallNbconvert()
+	}
+}
